6aa1a083071e2ef77aefb9eecc270308d067eaae
sidrk01 <81598654+sidrk01@users.noreply.github.com>
2021-05-09 17:33:28 -0700

    Update sramk002_lab8_part1.c

--- Lab8_PWM/turnin/sramk002_lab8_part1.c
+++ Lab8_PWM/turnin/sramk002_lab8_part1.c
@@ -6,6 +6,8 @@
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
+ *
+ *	Demo Video - https://youtu.be/cfm4tQc_ddU
  */
 
 #include <avr/io.h>
